c8fa6d15286b48852499a8a83dc4d5f37581da63
sayno <418215971@qq.com>
2021-12-24 13:56:45 +0800

    add function knowledge

--- basic/restart.py
+++ basic/restart.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from abc import ABC, abstractmethod
 from collections import namedtuple
 
 
@@ -33,43 +32,30 @@ class Order:
         if self.promotion is None:
             discount = 0
         else:
-            discount = self.promotion.discount(self)
+            discount = self.promotion(self)
         return self.total() - discount
 
     def __repr__(self):
         return f"<Order total: {self.total()} due: {self.due()}>"
 
 
-class Promotion(ABC):
-
-    @abstractmethod
-    def discount(self):
-        pass
+def fidelityPromo(order):
+    return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
 
 
-class FidelityPromo(Promotion):
-
-    def discount(self, order):
-        return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
-
-
-class BulkItemPromotion(Promotion):
-
-    def discount(self, order):
-        discount = 0
-        for item in order.cart:
-            if item.quantity > 20:
-                discount += item.total() * 0.1
-        return discount
+def bulkItemPromotion(order):
+    discount = 0
+    for item in order.cart:
+        if item.quantity > 20:
+            discount += item.total() * 0.1
+    return discount
 
 
-class LargeOrderPromotion(Promotion):
-
-    def discount(self, order):
-        distinct_item = [item.product for item in order.cart]
-        if len(distinct_item) >= 10:
-            return order.total() * 0.07
-        return 0
+def largeOrderPromotion(order):
+    distinct_item = [item.product for item in order.cart]
+    if len(distinct_item) >= 10:
+        return order.total() * 0.07
+    return 0
 
 
 if __name__ == '__main__':
@@ -78,5 +64,5 @@ if __name__ == '__main__':
     cart = [LineItem('banana', 4, 0.5),
             LineItem('apple', 10, 1.5),
             LineItem('watermellon', 5, 5)]
-    print(Order(A, cart, FidelityPromo()))
-    print(Order(B, cart, FidelityPromo()))
+    print(Order(A, cart, fidelityPromo))
+    print(Order(B, cart, fidelityPromo))
